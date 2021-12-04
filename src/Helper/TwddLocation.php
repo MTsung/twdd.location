4818df0d029fd0bdfb1460e6e7c90ffc47378114
MTsung <money015037@gmail.com>
2021-12-04 16:28:29 +0800

    取得可能的縣市

--- src/Helper/TwddLocation.php
+++ src/Helper/TwddLocation.php
@@ -1,15 +1,33 @@
 <?php
 namespace Mtsung\TwddLocation\Helper;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
+
 class TwddLocation
 {
     public function __construct()
     {
-        dd('hi');
+        
     }
 
-    public function aa()
+    /**
+     * 取得可能的縣市
+     *
+     * @param [type] $lat
+     * @param [type] $lng
+     * @return Collection
+     */
+    public function getPossibleCitys($lat, $lng): Collection
     {
-
+        $twCityRange = Storage::get(Config::get('twdd-location.output.tw_city_range'));
+        $twCityRange = json_decode($twCityRange);
+        $twCityRange = collect($twCityRange);
+        return $twCityRange
+            ->where('limit_location.min_lat', '<=', $lat)
+            ->where('limit_location.min_lng', '<=', $lng)
+            ->where('limit_location.max_lat', '>=', $lat)
+            ->where('limit_location.max_lng', '>=', $lng);
     }
 }
